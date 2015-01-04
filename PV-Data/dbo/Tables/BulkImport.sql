e3fb16f572d484b68602ff75587b77e470149a4b
Markus Kepert <mn@dieitexperten.de>
2015-01-04 23:22:02 +0100

    intial database import

---
+++ PV-Data/dbo/Tables/BulkImport.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[BulkImport] (
+    [DSIDSYS]   BIGINT        IDENTITY (1, 1) NOT NULL,
+    [TimeStamp] DATETIME      NOT NULL,
+    [Key]       NVARCHAR (50) NOT NULL,
+    [First]     FLOAT (53)    NULL,
+    [Last]      FLOAT (53)    NULL,
+    [Min]       FLOAT (53)    NULL,
+    [Max]       FLOAT (53)    NULL,
+    [Base]      INT           NULL,
+    [Period]    INT           NULL,
+    [Mean]      FLOAT (53)    NULL,
+    CONSTRAINT [PK_BulkImport] PRIMARY KEY CLUSTERED ([DSIDSYS] ASC)
+);
+
---
+++ PV-Data/dbo/Tables/BulkImport_Info.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[BulkImport_Info] (
+    [DSIDSYS] BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Created] DATETIME      NOT NULL,
+    [culture] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_BulkImport_Info] PRIMARY KEY CLUSTERED ([DSIDSYS] ASC)
+);
+
