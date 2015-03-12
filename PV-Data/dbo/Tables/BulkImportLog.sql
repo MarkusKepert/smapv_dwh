54466fbf64dabf50949061b988ec24d3e16fcd64
Markus Kepert <mn@dieitexperten.de>
2015-03-12 22:40:20 +0100

    logging created

---
+++ PV-Data/dbo/Tables/BulkImportLog.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[BulkImportLog]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SourceFileName] NVARCHAR(MAX) NOT NULL, 
+    [dtImportStart] DATETIME2 NOT NULL, 
+    [dtImportEnd] DATETIME2 NULL
+)
