f5bb0c72004eed9b2925352f0c3e8c6debb35ed5
Markus Kepert <mn@dieitexperten.de>
2015-06-26 23:08:52 +0200

    work on package info xml import

--- PV-Data/dbo/Tables/BulkImport_Info.sql
+++ PV-Data/dbo/Tables/BulkImport_Info.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[BulkImport_Info] (
     [DSIDSYS] BIGINT        IDENTITY (1, 1) NOT NULL,
-    [Created] DATETIME      NOT NULL,
-    [culture] NVARCHAR (50) NOT NULL,
+    [dtCreate] DATETIME      NOT NULL,
+    [SerialNumber] INT NOT NULL, 
+    [MacAddress] NCHAR(255) NOT NULL, 
+    [Created] DATETIME NOT NULL, 
+    [Version] NCHAR(255) NOT NULL, 
+    [OSVersion] NCHAR(255) NOT NULL, 
     CONSTRAINT [PK_BulkImport_Info] PRIMARY KEY CLUSTERED ([DSIDSYS] ASC)
 );
 
