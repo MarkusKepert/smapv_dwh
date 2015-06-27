34efbb1544ed395aaed74ec3958676c94aac5393
Markus Kepert <mn@dieitexperten.de>
2015-06-27 22:58:32 +0200

    database completed (add log data table

---
+++ PV-Data/dbo/Tables/BulkImport_Log.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[BulkImport_Log]
+(
+	[DSIDSYS] BIGINT NOT NULL PRIMARY KEY, 
+    [dtCreate] DATETIME NOT NULL, 
+    [DSID_PackageInfo] BIGINT NOT NULL, 
+    [Key] DATETIME NULL, 
+    [DateTime] DATETIME NULL, 
+    [EventType] NVARCHAR(255) NULL, 
+    [AccessLevel] NVARCHAR(255) NULL, 
+    [Category] NVARCHAR(255) NULL, 
+    [Device] NVARCHAR(255) NULL, 
+    [Module] NVARCHAR(255) NULL, 
+    [MessageCode] INT NULL, 
+    [MessageArgs] NVARCHAR(255) NULL, 
+    [Message] NVARCHAR(MAX) NULL
+)
